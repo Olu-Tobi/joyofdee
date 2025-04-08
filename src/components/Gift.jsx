f010a93fd4dcfe96ef7442e9e774d0a162f1c2c6
OluTobi-dev <prepdevs@gmail.com>
2025-04-08 11:22:55 +0100

    added account

--- src/components/Gift.jsx
+++ src/components/Gift.jsx
@@ -1,5 +1,8 @@
 import styled from "styled-components";
 import { GoGift } from "react-icons/go";
+import { BsCopy } from "react-icons/bs";
+import { useEffect, useState } from "react";
+import Popup from "./Popup";
 
 const Div = styled.div`
   position: relative;
@@ -122,7 +125,193 @@ const Btn = styled.button`
   }
 `;
 
+const PopDiv = styled.div`
+  margin: 0 auto;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  gap: 1.2rem;
+  height: 90%;
+  position: relative;
+
+  @media screen and (max-width: 748px) {
+    width: 90%;
+    justify-content: flex-start;
+    margin-top: 5rem;
+  }
+
+  @media screen and (max-width: 390px) {
+    width: 90%;
+    justify-content: flex-start;
+    margin-top: 2.5rem;
+  }
+`;
+const PopH2 = styled.h2`
+  font-size: 2.4rem;
+  text-align: center;
+  width: 75%;
+
+  @media screen and (max-width: 748px) {
+    font-size: 1.5rem;
+  }
+`;
+
+const PopList = styled.div`
+  display: flex;
+  align-items: flex-start;
+  justify-content: center;
+  gap: 2rem;
+  width: 95%;
+  padding-top: 2rem;
+
+  @media screen and (max-width: 748px) {
+    flex-direction: column;
+    align-items: flex-start;
+    padding-top: 2rem;
+    width: 90%;
+    gap: 0.6rem;
+  }
+
+  @media screen and (max-width: 390px) {
+    flex-direction: column;
+    align-items: flex-start;
+    padding-top: unset;
+    width: 90%;
+    gap: 0.6rem;
+  }
+`;
+const PopItem = styled.div`
+  background: #060a9020;
+  width: 8rem;
+  height: 9rem;
+  display: flex;
+  flex-direction: column;
+  gap: 1.3rem;
+  padding: 2rem 1rem;
+
+  @media screen and (max-width: 748px) {
+    width: 17.6rem;
+    height: 5rem;
+    padding: 1rem;
+    gap: 0.7rem;
+    flex-direction: row;
+    align-items: flex-start;
+    justify-content: space-between;
+  }
+
+  @media screen and (max-width: 390px) {
+    width: 17rem;
+    height: 4.5rem;
+    padding: 1rem;
+    gap: 0.4rem;
+    flex-direction: row;
+    align-items: flex-start;
+    justify-content: space-between;
+  }
+`;
+
+const PopTop = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 1.3rem;
+
+  @media screen and (max-width: 748px) {
+    gap: 0.6rem;
+  }
+
+  @media screen and (max-width: 390px) {
+    gap: 0.4rem;
+  }
+`;
+
+const ItemH2 = styled.h2`
+  font-size: 1rem;
+  font-weight: 800;
+`;
+
+const PopText = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+const ItemP = styled.p`
+  font-size: 0.9rem;
+  height: 1.5rem;
+`;
+const ItemBtn = styled.button`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 0.7rem;
+  background: #060a90;
+  color: #000;
+  font-weight: 600;
+  cursor: pointer;
+  border: none;
+
+  width: 7rem;
+  height: 2rem;
+  box-shadow: 1px 1px white, 2px 2px #060a90;
+
+  font-family: inherit;
+  position: relative;
+  color: #fff;
+
+  &::before,
+  &::after {
+    content: "";
+    position: absolute;
+    background: #060a90;
+  }
+  &::before {
+    top: 100%;
+    left: 1px;
+    height: 2px;
+    width: 1px;
+  }
+  &::after {
+    left: 100%;
+    top: 1px;
+    height: 1px;
+    width: 2px;
+  }
+
+  @media screen and (max-width: 748px) {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    width: 4rem;
+    height: 3.6rem;
+    gap: 0.4rem;
+  }
+`;
+const PopFoot = styled.p`
+  position: absolute;
+  bottom: 0;
+  font-size: 2rem;
+  font-weight: 500;
+  @media screen and (max-width: 748px) {
+    bottom: 2rem;
+  }
+
+  @media screen and (max-width: 390px) {
+    font-size: 1.2rem;
+    bottom: -1rem;
+  }
+`;
+
 const Gift = () => {
+  const [isVisible, setIsVisible] = useState(false); // state to control popup
+
+  const handleClick = () => {
+    setIsVisible(true); // show popup when Bank Transfer button is clicked
+  };
+
+  const handleClose = () => {
+    setIsVisible(false); // hide popup when close icon is clicked
+  };
   return (
     <Div id="gift">
       <PBack src="/back.webp" alt="background" />
@@ -146,14 +335,7 @@ const Gift = () => {
               color="#000"
               back="#060a90"
               style={{ boxShadow: "1px 1px white, 2px 2px #060a90" }}
-              onClick={() => {
-                navigator.clipboard.writeText(
-                  "Send Your Cash Gift to the account below : 2149751032 UBA Oyeleye David Dotun"
-                );
-                alert(
-                  "Copied: Send Your Cash Gift to the account below : 2149751032 UBA Oyeleye David Dotun"
-                );
-              }}
+              onClick={handleClick}
             >
               Bank Transfer
             </Btn>
@@ -168,6 +350,63 @@ const Gift = () => {
               </Btn>
             </a>
           </BtnDiv>
+
+          {isVisible && (
+            <Popup handleClose={handleClose}>
+              <PopDiv>
+                <PopH2>
+                  Please send your gift to any of the options below:
+                </PopH2>
+
+                <PopList>
+                  <PopItem>
+                    <PopTop>
+                      <ItemH2>UBA</ItemH2>
+                      <PopText>
+                        <ItemP>2149751032</ItemP>
+                        <ItemP>Oyeleye David Dotun</ItemP>
+                      </PopText>
+                    </PopTop>
+                    <ItemBtn
+                      onClick={() => {
+                        navigator.clipboard.writeText(
+                          "2149751032 - Oyeleye David Dotun (UBA)"
+                        );
+                        alert("Copied: 2149751032 - Oyeleye David Dotun (UBA)");
+                      }}
+                    >
+                      <BsCopy />
+                      Copy
+                    </ItemBtn>
+                  </PopItem>
+
+                  <PopItem>
+                    <PopTop>
+                      <ItemH2>Moniepoint</ItemH2>
+                      <PopText>
+                        <ItemP>7068115869</ItemP>
+                        <ItemP>Addeyanju Ayomide Esther</ItemP>
+                      </PopText>
+                    </PopTop>
+                    <ItemBtn
+                      onClick={() => {
+                        navigator.clipboard.writeText(
+                          "7068115869 - Addeyanju Ayomide Estheri (Moniepoint)"
+                        );
+                        alert(
+                          "Copied: 7068115869 - Addeyanju Ayomide Estheri (Moniepoint)"
+                        );
+                      }}
+                    >
+                      <BsCopy />
+                      Copy
+                    </ItemBtn>
+                  </PopItem>
+                </PopList>
+                <PopFoot>God Bless You!</PopFoot>
+              </PopDiv>
+            </Popup>
+          )}
         </Header>
       </Wrapper>
     </Div>
