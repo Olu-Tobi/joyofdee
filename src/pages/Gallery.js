82adf7c61e76df785310612934c2b323bd99f24b
OluTobi-dev <prepdevs@gmail.com>
2025-04-08 08:38:51 +0100

    new edits 2

--- src/pages/Gallery.js
+++ src/pages/Gallery.js
@@ -2,10 +2,12 @@ import PreShow2 from "../components/Preshow2";
 import Navbar from "../components/Navbar";
 import PreIntro from "../components/PreIntro";
 import Footer from "../components/Footer";
+import TimerEle from "../components/TimerEle";
 
 export default function Gallery() {
   return (
     <>
+      <TimerEle />
       <Navbar />
       <PreIntro />
       <PreShow2 />
