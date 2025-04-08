0d88b513de2b91c3f3e0d842d5aba217a83e74a8
OluTobi-dev <prepdevs@gmail.com>
2025-04-08 13:54:37 +0100

    updated vid

--- src/components/Proposal.jsx
+++ src/components/Proposal.jsx
@@ -152,9 +152,9 @@ const Proposal = () => {
           <CloseButton onClick={handleClose}>×</CloseButton>
           <VideoWrapper>
             <VimeoEmbed
-              src="https://player.vimeo.com/video/1072158236?h=d4e03650df&autoplay=1&title=0&byline=0&portrait=0&badge=0&autopause=0&player_id=0&app_id=58479"
-              allow="autoplay; fullscreen; picture-in-picture; clipboard-write; encrypted-media"
+              src="https://www.youtube.com/embed/shbZVh_xa1I?autoplay=1&rel=0"
               title="proposal"
+              allow="autoplay; encrypted-media; fullscreen"
               allowFullScreen
             />
           </VideoWrapper>
@@ -165,98 +165,3 @@ const Proposal = () => {
 };
 
 export default Proposal;
-
-// import styled from "styled-components";
-
-// const Div = styled.div`
-//   position: relative;
-//   padding: 4rem 0 2rem;
-//   display: flex;
-//   flex-direction: column;
-//   gap: 3rem;
-
-//   @media screen and (max-width: 748px) {
-//     padding: 2rem 0;
-//   }
-// `;
-
-// const Wrapper = styled.div`
-//   width: 60%;
-//   margin: 0 auto;
-//   display: flex;
-//   flex-direction: column;
-//   align-items: center;
-
-//   gap: 3rem;
-
-//   @media screen and (max-width: 748px) {
-//     width: 90%;
-//     flex-direction: column-reverse;
-//   }
-// `;
-
-// const H2 = styled.h2`
-//   font-size: 2.7rem;
-//   font-weight: 400;
-//   width: 20rem;
-//   text-align: center;
-
-//   @media screen and (max-width: 1280px) {
-//     font-size: 2.2rem;
-//     width: 16rem;
-//   }
-// `;
-
-// const ThumbDiv = styled.div`
-//   position: relative;
-//   width: 100%;
-//   height: 25rem;
-// `;
-
-// const Thumb = styled.img`
-//   width: 100%;
-//   height: 25rem;
-//   object-fit: cover;
-//   border-radius: 1rem;
-// `;
-// const Layer = styled.div`
-//   position: absolute;
-//   top: 0;
-//   left: 0;
-//   background: #00000050;
-//   width: 100%;
-//   height: 25rem;
-//   border-radius: 1rem;
-// `;
-
-// const VideoWrapper = styled.div`
-//   position: relative;
-//   width: 20rem;
-//   height: 25rem;
-// `;
-
-// const VimeoEmbed = styled.iframe`
-//   position: absolute;
-//   top: 0;
-//   left: 0;
-//   width: 100%;
-//   height: 100%;
-//   border-radius: 10px;
-//   //box-shadow: 3px 3px 15px rgba(0, 0, 0, 0.2);
-// `;
-
-// const Proposal = () => {
-//   return (
-//     <Div>
-//       <Wrapper>
-//         <H2 className="ds-font">The Moment Ayomide Said YES!</H2>
-//         <ThumbDiv>
-//           <Thumb src="/prewed16.webp" alt="img" />
-//           <Layer />
-//         </ThumbDiv>
-//       </Wrapper>
-//     </Div>
-//   );
-// };
-
-// export default Proposal;
